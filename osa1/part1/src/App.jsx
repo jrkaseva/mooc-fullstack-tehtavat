78a828c53ff4775a8ddebe5a093441c7a98a7f32
jrkaseva <joakim.kaseva@gmail.com>
2024-10-10 19:36:19 +0300

    Update App.jsx
    luennon seuraamista

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -1,39 +1,50 @@
 import { useState } from 'react'
 
-const Display = (props) => {
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
   return (
-    <div>{props.counter}</div>
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
   )
 }
 
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
-
   const [allClicks, setAll] = useState([])
 
+  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
-    setLeft(left + 1)
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
   }
-
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
-    setRight(right + 1)
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(left + updatedRight)
   }
 
   return (
     <div>
-      <div>
-        {left}
-        <button onClick={handleLeftClick}>left</button>
-        <button onClick={handleRightClick}>right</button>
-        {right}
-
-        <p>{allClicks.join(' ')}</p>
-      </div>
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
